86bfd3a2be3eceae8b03bb176592ba86dcb89a58
Jayden Shaw <jayden.shaw@gmail.com>
2020-12-11 09:50:04 -0700

    Debugging

---
+++ notes/9_debugging/debugging.py
@@ -0,0 +1,19 @@
+# debugging
+
+# linting
+# ide / editor
+    # highlighting/editing
+# reading errors yourself 
+
+# pdb
+# python debugger 
+# built in package 
+
+import pdb 
+# help in console for arguments
+
+def add(num1, num2):
+    pdb.set_trace()
+    return num1 + num2
+
+add(4, 'jk lol')
