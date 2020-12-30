d3337112bdf27ffc4b7f41dbaf0329b716e2ffbe
Jayden Shaw <jayden.shaw@gmail.com>
2020-12-30 09:11:14 -0700

    Unit testing section completed

---
+++ exercise/20_unit_testing/main.py
@@ -0,0 +1,21 @@
+import random 
+
+def run_guess(guess, answer):
+    if 0 < guess < 11:
+        if guess == answer:
+            print('Correct!')
+            return True 
+    else:
+        print('Please enter a number')
+        return False 
+    
+    if __name__ == '__main__':
+        answer = random.randint(1, 10)
+        while True:
+            try:
+                guess = int(input('Guess a number 1 - 10: '))
+                if (run_guess(guess, answer)):
+                    break
+            except: 
+                print('Please enter a number')
+                continue
---
+++ notes/12_testing/main.py
@@ -0,0 +1,10 @@
+# for unit testing
+
+def do_stuff(num):
+    try:
+        if num:
+            return int(num) + 5
+        else:
+            return 'please enter number'
+    except ValueError as err:
+        return err
